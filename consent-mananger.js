633040d1d650d18de539d6cdeff366e00c825bba
Taylor <93610040+TO-Point@users.noreply.github.com>
2023-05-12 12:50:09 -0400

    Create consent-mananger.js

---
+++ consent-mananger.js
@@ -0,0 +1,32 @@
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+// Get the element with the 'transcend-opt-out' id
+const optOutElement = document.getElementById('transcend-opt-out');
+
+// Hide the element by default
+optOutElement.style.display = 'none';
+
+// Check if the user falls under the CPRA regime
+if (airgap.getRegimes().has('CPRA')) {
+    optOutElement.style.display = 'block';
+
+    // Set the initial text
+    optOutElement.textContent = 'Do not sell or share my personal information.';
+
+    // Add a click event listener
+    optOutElement.addEventListener('click', function() {
+    // Open the Transcend Consent Manager with the CompleteOptions view
+    transcend.showConsentManager({ viewState: 'CompleteOptions' });
+    });
+    airgap.addEventListener(
+    'consent-change',
+    ({ detail: { consent, oldConsent, changes } }) => {
+        // Check if changes object is not null and if the consent has changed
+        if (changes !== null) {
+        optOutElement.textContent = 'We have updated your personal information preferences.';
+        }
+    }
+    );
+}
+});
