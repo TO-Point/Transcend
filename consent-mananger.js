5f4792aa134ff8bc08c29aba4dc2d8da8366875e
Taylor <93610040+TO-Point@users.noreply.github.com>
2023-05-15 20:51:59 -0400

    Adds detection for CM states before showing message
    This complicates the UI a bit. We know check if they have opted out of X, and show unique messages accordingly.

--- consent-mananger.js
+++ consent-mananger.js
@@ -1,31 +1,41 @@
-// Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', function() {
-// Get the element with the 'transcend-opt-out' id
-const optOutElement = document.getElementById('transcend-opt-out');
+  // Wait for the DOM to load
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the element with the 'transcend-opt-out' id
+    const optOutElement = document.getElementById('transcend-opt-out');
 
-// Hide the element by default
-optOutElement.style.display = 'none';
+    // Hide the element by default
+    optOutElement.style.display = 'none';
 
-// Check if the user falls under the CPRA regime
-if (airgap.getRegimes().has('CPRA')) {
-    optOutElement.style.display = 'block';
+    // Check if the user falls under the CPRA regime
+    if (airgap.getRegimes().has('CPRA')) {
+      // Always display the opt-out element for users in the CPRA regime
+      optOutElement.style.display = 'block';
 
-    // Set the initial text
-    optOutElement.textContent = 'Manage how Point handles your personal information.';
+      // Fetch the current consent status
+      const consent = airgap.getConsent();
 
-    // Add a click event listener
-    optOutElement.addEventListener('click', function() {
-    // Open the Transcend Consent Manager with the CompleteOptions view
-    transcend.showConsentManager({ viewState: 'CompleteOptions' });
-    });
-    airgap.addEventListener(
-    'consent-change',
-    ({ detail: { consent, oldConsent, changes } }) => {
-        // Check if changes object is not null and if the consent has changed
-        if (changes !== null) {
-        optOutElement.textContent = 'We have updated your personal information preferences.';
+      // Check the SaleOfInfo value and set the text accordingly
+      if (consent.purposes.SaleOfInfo) {
+        optOutElement.textContent = 'Do Not Sell or Share My Personal Information.';
+      } else {
+        optOutElement.textContent = 'We No Longer Sell or Share Your Personal Information.';
+      }
+
+      // Add a click event listener
+      optOutElement.addEventListener('click', function() {
+        // Open the Transcend Consent Manager with the CompleteOptions view
+        transcend.showConsentManager({ viewState: 'CompleteOptions' });
+      });
+
+      // Listen for changes in consent
+      airgap.addEventListener(
+        'consent-change',
+        ({ detail: { consent, oldConsent, changes } }) => {
+          // Check if changes object is not null and if the consent has changed
+          if (changes !== null) {
+            optOutElement.textContent = 'Your privacy choices have been updated.';
+          }
         }
+      );
     }
-    );
-}
-});
+  });
