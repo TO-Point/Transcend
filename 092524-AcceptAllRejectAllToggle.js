37ac65fdceb53e53f3b00d22653cb885bedff9c1
Taylor <93610040+TO-Point@users.noreply.github.com>
2024-09-27 15:02:43 -0700

    Create 092524-AcceptAllRejectAllToggle.js

---
+++ 092524-AcceptAllRejectAllToggle.js
@@ -0,0 +1,41 @@
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the element with the 'transcend-opt-out' id
+  const optOutElement = document.getElementById('transcend-opt-out');
+
+  // Hide the element by default
+  optOutElement.style.display = 'none';
+
+  // Check if the user falls under the CPRA regime
+  if (airgap.getRegimes().has('CPRA')) {
+    // Always display the opt-out element for users in the CPRA regime
+    optOutElement.style.display = 'flex';
+
+    // Fetch the current consent status
+    const consent = airgap.getConsent();
+
+    // Check the SaleOfInfo and Advertising values and set text
+    if (consent.purposes.Advertising === false) {
+      optOutElement.textContent = 'We No Longer Sell or Share Your Personal Information.';
+    } else {
+      optOutElement.textContent = 'Your Privacy Choices';
+    }
+
+    // Add a click event listener
+    optOutElement.addEventListener('click', function() {
+      // Open the Transcend Consent Manager with the CompleteOptions view
+      transcend.showConsentManager({ viewState: 'AcceptAllRejectAllToggle' });
+    });
+
+    // Listen for changes in consent
+    airgap.addEventListener(
+      'consent-change',
+      ({ detail: { consent, oldConsent, changes } }) => {
+        // Check if changes object is not null and if the consent has changed
+        if (changes !== null) {
+          optOutElement.textContent = 'Your privacy choices have been updated.';
+        }
+      }
+    );
+  }
+});
